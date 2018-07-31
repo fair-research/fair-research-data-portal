c44202f3c82e920a419804389bfb7f644d7560b7
Nickolaus D. Saint <nick@globus.org>
2018-07-31 14:42:16 -0700

    Fixed URLs for mounting on july-demo

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include, re_path
+from django.urls import path, include, re_path, reverse
+from django.shortcuts import redirect
 from django.contrib import admin
 
 
@@ -8,17 +9,22 @@ from portal.views import (landing_page, bag_create, workflows, tasks,
 
 from portal.api import task_start, update_tasks
 
+PREFIX = 'july-demo/'
+
 apipatterns = [
     path('task/start/', task_start, name='task-start'),
     path('tasks/update/', update_tasks, name='tasks-update'),
     # path('tasks/active', get_running_tasks, name='tasks-active')
 ]
 
-urlpatterns = [
+general = [
+    path('fair-portal-login/',
+         lambda r: redirect(reverse('login') + 'globus?next=/' + r.get_full_path()),
+         name='fair-portal-login'),
+    path('fair-portal-logout/',
+         lambda r: redirect(reverse('logout') + '?next=/' + PREFIX),
+         name='fair-portal-logout'),
     path('admin/', admin.site.urls),
-    path('', include('social_django.urls')),
-    path('', include('django.contrib.auth.urls')),
-    path('api/v1/', include(apipatterns)),
     path('profile/', profile, name='profile'),
     path('bags/', workflows, name='bag-list'),
     path('workflows', workflows, name='workflows'),
@@ -30,8 +36,20 @@ urlpatterns = [
     path('tasks', tasks, name='tasks'),
     path('search/bags/delete/<path:minid>/', bag_delete, name='bag-delete'),
     path('search/bags/add', bag_add, name='bag-add'),
-    path('search/', include('globus_portal_framework.search.urls')),
+    # path('search/', include('globus_portal_framework.search.urls')),
 
-    path('', include('globus_portal_framework.search.urls'), name='landing_page')
+
     # path('', landing_page, name='landing_page')
+]
+
+urlpatterns = [
+    path('accounts/profile/', lambda r: redirect('/'+PREFIX)),
+    path(PREFIX, include('social_django.urls')),
+    path(PREFIX, include('django.contrib.auth.urls')),
+    # path()
+    path(PREFIX + 'api/v1/', include(apipatterns)),
+    path(PREFIX, include(general)),
+    path(PREFIX, include('globus_portal_framework.search.urls'),
+         name='landing_page'),
+
 ]
